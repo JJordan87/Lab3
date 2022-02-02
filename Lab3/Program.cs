234f25c57b92908e985d6b4fa5de29c3050bd6b8
Jon <JJKing619@gmail.com>
2022-02-02 15:14:33 -0500

    first commit

---
+++ Lab3/Program.cs
@@ -0,0 +1,63 @@
+﻿
+
+bool runProgram = true;
+string userGoAgain;
+int i;
+
+Console.WriteLine("Learn your squares & cubes!");
+while(true)
+{
+    Console.Write("Enter a integer between 1 and 1289: ");
+    int choosenNum = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+
+    if (choosenNum <= 0)
+    {
+        Console.WriteLine("Please choose a positive integer.");
+        continue;
+    }
+    else if (choosenNum > 1290)
+    {
+        Console.WriteLine("Please select a smaller number");
+        continue;
+    }
+
+    Console.WriteLine(String.Format("{0,6} {1,12} {2,20}\n ", "Number", "Squared", "Cubed"));
+    Console.WriteLine(String.Format("{0,6} {1,12} {2,20}\n ", "=======", "=======", "======"));
+    for (i = 1; i <= choosenNum; i++)
+    {
+        int square = (int)Math.Pow(i, 2);
+        int cube = (int)Math.Pow(i, 3);
+        Console.WriteLine(String.Format("{0,6} {1,12} {2,20}\n ", i, square, cube));
+    }
+    if (i >= choosenNum)
+    {
+        Console.WriteLine();
+        Console.WriteLine("Would you like to run the program again? Enter [y]or[n]");
+        userGoAgain = Console.ReadLine().ToLower();
+        if (userGoAgain == "y")
+        {
+            runProgram = true;
+            continue;
+        }
+        else if (userGoAgain == "n")
+        {
+            runProgram = false;
+            break;
+        }
+        else
+        {
+            Console.WriteLine("not a valid input");
+        }
+    }
+}
+
+//methods ended not using 
+//static int cubed(int x)
+//{
+//    return (int)Math.Pow(numCub, 3);
+//} 
+//static int squared(int numSq)
+//{
+//    return (int)Math.Pow(numSq, 2);
+//}
